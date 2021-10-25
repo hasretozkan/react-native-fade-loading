278292a2bb25ffc8b4f672d3b2aa78a23dc1189e
Hasret Özkan <hasretozkan1@gmail.com>
2021-10-26 00:19:16 +0300

    0.0.1 -> demo release

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,12 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-fade-loading';
+import { FadeLoading } from 'react-native-fade-loading';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <FadeLoading style={styles.box} />
     </View>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,76 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+import React, { ReactChild } from 'react';
+import { StyleSheet, Animated, ViewStyle } from 'react-native';
+
+// FadeLoading Props Types
+interface Props {
+  style: ViewStyle;
+  children: ReactChild;
+  primaryColor: string;
+  secondaryColor: string;
+  duration: number;
 }
+
+// FadeLoading State Types
+interface State {
+  cardAnimation: Animated.Value;
+}
+
+// FadeLoading Default Props Values
+
+export class FadeLoading extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      cardAnimation: new Animated.Value(0),
+    };
+  }
+
+  componentDidMount = async () => {
+    this.handleAnimation();
+  };
+
+  handleAnimation = () => {
+    const { cardAnimation } = this.state;
+    const { duration = 2000 } = this.props;
+
+    Animated.timing(cardAnimation, {
+      toValue: 1,
+      duration: duration / 2,
+      useNativeDriver: false,
+    }).start(() => {
+      Animated.timing(cardAnimation, {
+        toValue: 0,
+        duration: duration / 2,
+        useNativeDriver: false,
+      }).start(() => this.handleAnimation());
+    });
+  };
+
+  render() {
+    const { cardAnimation } = this.state;
+    const {
+      primaryColor = 'whitesmoke',
+      secondaryColor = 'lightgray',
+      style = {},
+      children,
+    } = this.props;
+
+    const backgroundColor = cardAnimation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [primaryColor, secondaryColor],
+    });
+
+    return (
+      <Animated.View style={[styles.fadeLoading, { backgroundColor }, style]}>
+        {children}
+      </Animated.View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  fadeLoading: {
+    padding: 5,
+    borderRadius: 4,
+  },
+});
